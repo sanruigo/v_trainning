53c48267350506ec4a877678aa4882fb72f29c75
Jorge Zambrano <jorgenluis004@hotmail.com>
2013-12-01 17:02:40 +0100

    plot

--- src/project/chart/ChartPlot.java
+++ src/project/chart/ChartPlot.java
@@ -1,6 +1,18 @@
+/**
+ * This class provides information about plot or different charts
+ *
+ * @author Sandra Ruiz
+ * @author 
+ * @version 1.0, 1/12/2013
+ * @since 1.4
+ */
+
+
 package project.chart;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Vector;
 
 import android.graphics.Color;
 
@@ -12,74 +24,73 @@ import com.androidplot.xy.XYSeries;
 
 public class ChartPlot {
 
-	private XYPlot mySimpleXYPlot;
+	//private XYPlot mySimpleXYPlot;
+	/**
+	 * Set of number for plot
+	 * */
 	private Number[] serie;
 	private XYSeries series;
 	private LineAndPointFormatter seriesFormat;
+	
+	
+	/**
+	 * Get all Numbers for Plot
+	 * @return set Numbers for Plot
+	 * */
 	public Number[] getSerie(){
 		return this.serie;
 	}
 	
-	public void setSerie1(Number[]serie){
+	
+	/**
+	 * Store or Set all number for plot
+	 * @param serie Set of number for the plot
+	 * */
+	public void setSerie(Number[]serie){
 		this.serie=serie;
 	}
 	
 	
-	// Añadimos Línea Número UNO:
-    // Exiten dos formatos del array
-    // 1.XY_VALS_INTERLEAVED => obtiene los valoes en pares es decir (1,8), (5,2) ,(7,4)
-    // 2.Y_VALS_ONLY => obtiene todo el array pero para valores en la ordenadas
+	/**
+	 * @return series XYSeries
+	 * 
+	 * */
 	public XYSeries getSeries(){
 		return this.series;
 	}
 	
+	
+	/**
+	 * In this method exist two types of Array Format:
+	 * 1.XY_VALS_INTERLEAVED => get values in pairs for abscissa and ordinate axes, (1,8), (5,2), (7,4)
+	 * 2.Y_VALS_ONLY => get values only in ordinate axes
+	 * @param serie Set of Number for Plot
+	 * @param name_serie Name of set data 
+	 * */
 	public void setXYSeries(Number[] serie,String name_serie){
 		this.series=new SimpleXYSeries(Arrays.asList(serie),  SimpleXYSeries.ArrayFormat.Y_VALS_ONLY,name_serie);
 		
 	}
-	
+
+	/**
+	 * Get the format of Series
+	 *@return seriesFormat LineAndPointFormatter 
+	 **/
 	public LineAndPointFormatter getSeriesFormat(){
 		return seriesFormat;
 	}
 	
+	
+	/**
+	 * Set a Format, Color for line, point and fill
+	 *@param linea color of line
+	 *@param punto color of point
+	 *@param relleno color of fill
+	 **/
 	public void setSeriesFormat(int linea,int punto,int relleno){
 			seriesFormat= new LineAndPointFormatter(linea,punto,relleno, null);
 	}
 	
-	public void chart(){
-
- 
-        // Creamos dos arrays de prueba. En el caso real debemos reemplazar
-        // estos datos por los que realmente queremos mostrar
-        //Number[] series1Numbers = {1, 8, 5, 2, 7, 4};
-        //Number[] series2Numbers = {4, 6, 3, 8, 2, 10};
- 
-        
-        //XYSeries series1 = 
-        
-        		// Array de datos
-                 // Sólo valores verticales
-                 // Nombre de la primera serie
- 
-        // Repetimos para la segunda serie
-        //XYSeries series2 = new SimpleXYSeries(Arrays.asList(series2Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series2");
- 
-        // Modificamos los colores de la primera serie
-           
-        // Color de la línea                   
-        // Color del punto
-         // Relleno
-        
-        
-        
-       
-        
- 
-        // Repetimos para la segunda serie
-      //  mySimpleXYPlot.addSeries(series2, new LineAndPointFormatter(Color.rgb(0, 0, 200), Color.rgb(0, 0, 100), Color.rgb(150, 150, 190), null));
- 
-
 	
-	}
 	
 }
--- src/project/v_trainning/MainActivity.java
+++ src/project/v_trainning/MainActivity.java
@@ -1,26 +1,29 @@
 package project.v_trainning;
 
-import com.androidplot.xy.XYPlot;
+import java.util.Arrays;
+import java.util.Vector;
 
 import project.chart.ChartPlot;
 import project.database.DataBase_vTrainning;
+
+import com.androidplot.xy.LineAndPointFormatter;
+import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.xy.XYPlot;
+import com.androidplot.xy.XYSeries;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.graphics.Color;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
-
-	
-	private XYPlot mySimpleXYPlot;//Variable para cargar el Plot
-	
+	private XYPlot mySimpleXYPlot;
+	public ChartPlot cp;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		plotExample();
-        
+		chartExample();
 		//dbExample();
 	}
 
@@ -30,80 +33,82 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
 	
 	
 	
-	public void plotExample(){
+	
+	public void chartExample(){
 		
-		// Creamos dos arrays de prueba. En el caso real debemos reemplazar
-        // estos datos por los que realmente queremos mostrar
-		Number[] series1Numbers = {1, 8, 5, 2, 7, 4};
-		ChartPlot chP=new ChartPlot();
-		chP.setSerie1(series1Numbers);
-		chP.setXYSeries(chP.getSerie(), "Serie");
-		chP.setSeriesFormat(Color.BLUE, Color.RED, 0);
-		
+		cp = new ChartPlot();
 		// Inicializamos el objeto XYPlot búscandolo desde el layout:
         mySimpleXYPlot = (XYPlot) findViewById(R.id.mySimpleXYPlot);
+ 
+        // Creamos dos arrays de prueba. En el caso real debemos reemplazar
+        // estos datos por los que realmente queremos mostrar
+        Number[] series1Numbers = {1, 8, 5, 2, 7, 4};
+        
+        
+        //Ingreso de datos
+        cp.setSerie(series1Numbers);
+        cp.setSeriesFormat(Color.BLACK, Color.RED,Color.GRAY);
+        cp.setXYSeries(cp.getSerie(), "Serie1");
+
         // Una vez definida la serie (datos y estilo), la añadimos al panel
-        mySimpleXYPlot.addSeries(chP.getSeries(), chP.getSeriesFormat());
-		
+        mySimpleXYPlot.addSeries(cp.getSeries(), cp.getSeriesFormat());
+      
+	
 	}
-	
-	
 	
 	public void dbExample(){
-		//Abrimos la base de datos 'DBUsuarios' en modo escritura
-		DataBase_vTrainning db =new DataBase_vTrainning(this, "DBvTrainning", null, 1);
-        //Inserta Usuarios
-        db.setUsuario("Jorge", 25, 60.4);
-//        db.setUsuario("Luis", 25, 60.4);
-//        db.setUsuario("Zambrano", 25, 60.4);
-        
-        
-        
-//////SESION 1        
-        //Inserta Sesion con sus repectivas informaciones
-       db.setSesion(db.getUsuarioName(1),"2013-05-30",120.0,60.0,1705.2,10.2,1019.0,"764");
-       
-        //Inserta la informacion GPS
-        db.setGPS(db.getUsuarioName(1),39.4805899205, -0.3472319210 ,1.75,10.2,0,0);
-        db.setGPS(db.getUsuarioName(1),39.4823495991, -0.3462663257 ,1.75,10.2,220.0,180);
-        db.setGPS(db.getUsuarioName(1),39.4828340204, -0.3461375797 ,1.75,10.2,59.0,44);
-        db.setGPS(db.getUsuarioName(1),39.4832418426, -0.3475081885 ,1.75,10.2,130.0,60);
-        db.setGPS(db.getUsuarioName(1),39.4826187224, -0.3491443360 ,1.75,10.2,160.0,120);
-        db.setGPS(db.getUsuarioName(1),39.4814925377, -0.3506302798 ,1.75,10.2,180.0,120);
-        db.setGPS(db.getUsuarioName(1),39.4805899205, -0.3472319210 ,1.75,10.2,300.0,240);
-        db.setGPS(db.getUsuarioName(1),39.4805899205, -0.3472319210 ,1.75,10.2,0,0);
-        
-//////////SESION 2
-        
-        //Inserta Sesion con sus repectivas informaciones
-        db.setSesion(db.getUsuarioName(1),"2013-05-31",121.0,62.0,1805.4,11.2,279.0,"205");
-      //Inserta la informacion GPS
-        db.setGPS(db.getUsuarioName(1),39.4805899205, -0.3472319210 ,1.75,11.2,0,0);
-        db.setGPS(db.getUsuarioName(1),39.4823495991, -0.3462663257 ,1.75,11.2,220.0,171);
-        db.setGPS(db.getUsuarioName(1),39.4828340204, -0.3461375797 ,1.75,11.2,59.0,34);
-        
-        System.out.println("All names:"+db.getAllUsuarioName().toString());
-        System.out.println("Usuario:"+db.getUsuarioName(1)+" Edad:"+db.getUsuarioAge(1)+" Peso:"+db.getUsuarioWeight(db.getUsuarioName(1)));
-        System.out.println("Sesion 1 (Fecha):"+db.getSesionPerIdSesion(db.getUsuarioName(1), 1,0));
-        System.out.println("Sesion por nombre"
-        		+ "\nFecha:"+db.getSesionPerUser(db.getUsuarioName(1),0)+""
-        		+ "\nbasal_sistolica:"+db.getSesionPerUser(db.getUsuarioName(1),1)+""
-      			+ "\nbasal_diastolica:"+db.getSesionPerUser(db.getUsuarioName(1),2)+""
-      			+ "\ncalorias_quemadas:"+db.getSesionPerUser(db.getUsuarioName(1),3)+""
-      			+ "\nvelocidad_media:"+db.getSesionPerUser(db.getUsuarioName(1),4)+""
-      			+ "\ndistancia_total:"+db.getSesionPerUser(db.getUsuarioName(1),5)+""
-      			+ "\ntiempo_total:"+db.getSesionPerUser(db.getUsuarioName(1),6)+"");
-        System.out.println("Sesion por Fecha (id): "+db.getSesionPerDate(db.getUsuarioName(1), "2013-05-31").toString());
-        System.out.println("GPS: "+db.getGPS(db.getUsuarioName(1), db.getSesionPerDate(db.getUsuarioName(1), "2013-05-31").get(0), 1).toString());
-	
-        System.out.println("inicio Github");
-        System.out.println("Otra linea para verificar cambios en github");
-	}
+        //Abrimos la base de datos 'DBUsuarios' en modo escritura
+        DataBase_vTrainning db =new DataBase_vTrainning(this, "DBvTrainning", null, 1);
+		//Inserta Usuarios
+		db.setUsuario("Jorge", 25, 60.4);
+		//db.setUsuario("Luis", 25, 60.4);
+		//db.setUsuario("Zambrano", 25, 60.4);
 
+
+
+		//////SESION 1        
+		//Inserta Sesion con sus repectivas informaciones
+		db.setSesion(db.getUsuarioName(1),"2013-05-30",120.0,60.0,1705.2,10.2,1019.0,"764");
+		
+		//Inserta la informacion GPS
+		db.setGPS(db.getUsuarioName(1),39.4805899205, -0.3472319210 ,1.75,10.2,0,0);
+		db.setGPS(db.getUsuarioName(1),39.4823495991, -0.3462663257 ,1.75,10.2,220.0,180);
+		db.setGPS(db.getUsuarioName(1),39.4828340204, -0.3461375797 ,1.75,10.2,59.0,44);
+		db.setGPS(db.getUsuarioName(1),39.4832418426, -0.3475081885 ,1.75,10.2,130.0,60);
+		db.setGPS(db.getUsuarioName(1),39.4826187224, -0.3491443360 ,1.75,10.2,160.0,120);
+		db.setGPS(db.getUsuarioName(1),39.4814925377, -0.3506302798 ,1.75,10.2,180.0,120);
+		db.setGPS(db.getUsuarioName(1),39.4805899205, -0.3472319210 ,1.75,10.2,300.0,240);
+		db.setGPS(db.getUsuarioName(1),39.4805899205, -0.3472319210 ,1.75,10.2,0,0);
+		
+		//////////SESION 2
+		
+		//Inserta Sesion con sus repectivas informaciones
+		db.setSesion(db.getUsuarioName(1),"2013-05-31",121.0,62.0,1805.4,11.2,279.0,"205");
+		//Inserta la informacion GPS
+		db.setGPS(db.getUsuarioName(1),39.4805899205, -0.3472319210 ,1.75,11.2,0,0);
+		db.setGPS(db.getUsuarioName(1),39.4823495991, -0.3462663257 ,1.75,11.2,220.0,171);
+		db.setGPS(db.getUsuarioName(1),39.4828340204, -0.3461375797 ,1.75,11.2,59.0,34);
+		
+		System.out.println("All names:"+db.getAllUsuarioName().toString());
+		System.out.println("Usuario:"+db.getUsuarioName(1)+" Edad:"+db.getUsuarioAge(1)+" Peso:"+db.getUsuarioWeight(db.getUsuarioName(1)));
+		System.out.println("Sesion 1 (Fecha):"+db.getSesionPerIdSesion(db.getUsuarioName(1), 1,0));
+		System.out.println("Sesion por nombre"
+		                + "\nFecha:"+db.getSesionPerUser(db.getUsuarioName(1),0)+""
+		                + "\nbasal_sistolica:"+db.getSesionPerUser(db.getUsuarioName(1),1)+""
+		                      + "\nbasal_diastolica:"+db.getSesionPerUser(db.getUsuarioName(1),2)+""
+		                      + "\ncalorias_quemadas:"+db.getSesionPerUser(db.getUsuarioName(1),3)+""
+		                      + "\nvelocidad_media:"+db.getSesionPerUser(db.getUsuarioName(1),4)+""
+		                      + "\ndistancia_total:"+db.getSesionPerUser(db.getUsuarioName(1),5)+""
+		                      + "\ntiempo_total:"+db.getSesionPerUser(db.getUsuarioName(1),6)+"");
+		System.out.println("Sesion por Fecha (id): "+db.getSesionPerDate(db.getUsuarioName(1), "2013-05-31").toString());
+		System.out.println("GPS: "+db.getGPS(db.getUsuarioName(1), db.getSesionPerDate(db.getUsuarioName(1), "2013-05-31").get(0), 1).toString());
+		
+		System.out.println("inicio Github");
+		System.out.println("Otra linea para verificar cambios en github");
+	}
 	
-	
+
 }
