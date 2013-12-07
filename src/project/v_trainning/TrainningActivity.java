7d2d37912a4d7b58351bf1e8dcbaddbd0ab7d7fa
Sandra <sanruigo@gmail.com>
2013-12-08 00:34:08 +0100

    actualizacion de actividades

--- src/project/v_trainning/TrainningActivity.java
+++ src/project/v_trainning/TrainningActivity.java
@@ -2,14 +2,17 @@ package project.v_trainning;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class TrainningActivity extends Activity {
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_trainning);
+		
 	}
 
 	@Override
@@ -19,4 +22,18 @@ public class TrainningActivity extends Activity {
 		return true;
 	}
 
+	/**
+	 * Method to launch Settings
+	 * */
+	public void launchSettingActivity(View view){
+		startActivity(new Intent(TrainningActivity.this,AjustesActivity.class));
+		
+	}
+	
+
+	public void launchResumeActivity(View view){
+		startActivity(new Intent(TrainningActivity.this,ResumenActivity.class));
+		
+	}
+	
 }
