bda53e9874249176de670102f0edd5389e21f6ad
mnaviam <mnaviam@gmail.com>
2014-01-04 19:52:52 +0100

    ensamblaje y detalles 0.1

--- src/project/v_trainning/TrainningActivity.java
+++ src/project/v_trainning/TrainningActivity.java
@@ -1,6 +1,7 @@
 package project.v_trainning;
 
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -10,15 +11,29 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import project.gps.*;
 
 public class TrainningActivity extends Activity {
-	
+	/***
+	 * @param mode, MYPREFS_SETTINGS
+	 * Variables estaticas del modo en que se guardarán las preferencias
+	 * */
 	  final int mode = Activity.MODE_PRIVATE;
 	  final String MYPREFS_SETTINGS = "MyPreferencesSettings";
 	  SharedPreferences myPreferences, myPreferencesRecover;
 	boolean isActivatedAjustes=false;
 	TextView actividad;
-	  
+	/***
+	 * @param isTrainingActive 
+	 * Variable que indica si esta activo el modo entrenamiento 
+	 * */ 
+	boolean isTrainingActive=false; //Agregado por Marlon
+	/**
+	 * @param TM
+	 * Se utiliza para el monitoreo de la actividad
+	 */ 
+	TrainingMonitor TM; //Agregado por Marlon  
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -83,11 +98,67 @@ public class TrainningActivity extends Activity {
 		myPreferencesRecover = getSharedPreferences(MYPREFS_SETTINGS,mode);
 		System.out.println(myPreferencesRecover.getString("nombre_actividad", "").toString());
 		actividad.setText(myPreferencesRecover.getString("nombre_actividad", "").toString());
+		TM=new TrainingMonitor((String) actividad.getText());
 		//actividad.setText(myPreferencesRecover.getString("nombre_actividad", ""));
 				
 
 	}
 	
-
+	/**
+	 * startTraining inicia o termina la función de entrenmiento
+	 * @param view
+	 */
+	private void startTraining(View view){
+		if (!isTrainingActive && isActivatedAjustes){
+			isTrainingActive=true;
+			TM.startTrainingMonitoring();
+		}
+		else{
+			isTrainingActive=false;
+			TM.showResults();
+		}
+		
+	}
 	
+	/**
+	 *
+	 * @author MNM
+	 * clase encargada del monitoreo de la actividad
+	 */
+	public class TrainingMonitor extends AsyncTask<Boolean,Integer,Boolean>{
+		
+		private String Actividad;
+		private GPS gps;
+		private boolean isActive;
+		
+		TrainingMonitor(String activ){
+			Actividad=activ;
+			gps = new GPS();
+			isActive=false;
+		}
+		
+		/**
+	 	* startTrainingMonitoring ejecuta el monitoreo secuencial del recorrido
+	 	*/
+		public void startTrainingMonitoring(){
+			isActive=true;
+			while (isActive){
+				
+			}
+		}
+	
+		/**
+	 	* showResults muestra los resultados al volver a pulsar el boton play
+	 	*/
+		public void showResults(){
+		
+		}
+
+		@Override
+		protected Boolean doInBackground(Boolean... params) {
+			// TODO Auto-generated method stub
+			return null;
+		}	
+	
+	}
 }
