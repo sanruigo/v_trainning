ee0815273c56e9c286da55bffda6b05d60d090c7
Jorge Zambrano <jorgenluis004@hotmail.com>
2014-01-08 17:34:24 +0100

    GPS en Trainning

--- src/project/v_trainning/TrainningActivity.java
+++ src/project/v_trainning/TrainningActivity.java
@@ -1,5 +1,7 @@
 package project.v_trainning;
 
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 
 import java.util.Vector;
@@ -22,8 +24,12 @@ import project.gps.*;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.SystemClock;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 
-public class TrainningActivity extends Activity {
+public class TrainningActivity extends Activity implements LocationListener{
 	/***
 	 * @param mode, MYPREFS_SETTINGS
 	 * Variables estaticas del modo en que se guardar�n las preferencias
@@ -74,6 +80,105 @@ public class TrainningActivity extends Activity {
 	long updatedTime = 0L;
 	private TextView txtTiempo;
 	private Runnable updateTimerThread;
+	
+	
+	
+	/***
+	 * 
+	 * GPS
+	 * 
+	 * */
+	
+	/***
+	 * @param E
+	 *            array de strings con posibles estados del gps
+	 */
+	private static final String[] E = { "fuera de servicio",
+			"temporalmente no disponible ", "disponible" };
+
+	/***
+	 * @param manejador
+	 *            manejador del gps
+	 */
+	private LocationManager manejador;
+
+	/***
+	 * @param salida
+	 *            para mostrar la salida en el textview
+	 */
+	//private TextView salida;
+
+	/***
+	 * @param pointsCuantity
+	 *            para llevar orden de obtencion de las coordenadas gps
+	 */
+	private int pointsCuantity;
+
+	/***
+	 * @param velocity
+	 *            almacena la velocidad en metros/segundo en cada punto de
+	 *            coordenadas obtenido
+	 */
+	private float velocity;
+
+	/***
+	 * @param latitude
+	 *            almacena la latitud en grados de cada punto de coordenadas
+	 *            obtenido
+	 */
+	private double latitude;
+
+	/***
+	 * @param longitude
+	 *            almacena la longitud en grados de cada punto de coordenadas
+	 *            obtenido
+	 */
+	private double longitude;
+
+	/***
+	 * @param time
+	 *            almacena el tiempo UTC en milisegundos desde el 1 de enero de
+	 *            1970 en cada punto de coordenadas obtenido
+	 */
+	private long time;
+
+	/***
+	 * @param altitude
+	 *            almacena la altura en metros desde el nivel del mar en cada
+	 *            punto de coordenadas obtenido
+	 */
+	private double altitude;
+
+	/***
+	 * @param distance
+	 *            almacena la distancia en metros entre el punto actual y el
+	 *            anterior
+	 */
+	private float distance;
+
+	/***
+	 * @param lastLoc
+	 *            almacena las coordenadas del punto anterior para poder
+	 *            calcular la distancia
+	 */
+	private Location lastLoc;
+
+	/***
+	 * @param minTime
+	 *            marca el tiempo minimo en milisegundos entre obtencion de
+	 *            coordenadas
+	 */
+	private int minTime;
+
+	/***
+	 * @param minDist
+	 *            marca la distancia minima en metros para la obtencion de
+	 *            coordenadas
+	 */
+	private int minDist;
+	
+	
+	
 	
 	
 	@Override
@@ -327,11 +432,8 @@ public class TrainningActivity extends Activity {
 					if(primerMultiplo){
 						System.out.println("GPSSSSSS");
 						
-						gps.localizador();
-						System.out.println("Distancia:"+gps.getLastDistance()+" "+gps.getLastSpeed());
-						//Velocidad.add(gps.getLastSpeed());
-						//Distancia.add(gps.getLastDistance());
-						
+						startGPS();
+						txtDistancia.setText(String.valueOf(getLastDistance()));
 						primerMultiplo=false;
 					}					
 					}else{
@@ -360,7 +462,128 @@ public class TrainningActivity extends Activity {
 		
 		
 	}
+
+	public void startGPS(){
+		
+		
+		pointsCuantity = 0;
+		minTime = 30000;
+		minDist = 1;
+
+		
+
+		manejador = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+		log("Esperando recepcion GPS");
+
+		
+		//Esta localizacion esta cacheada, no hay que mostrarla, es solo para comenzar mas rapido la recepcion GPS
+		Location lastKnownLocation = manejador.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+		muestraLocaliz(lastKnownLocation);
+
+		manejador.requestLocationUpdates(LocationManager.GPS_PROVIDER, minTime,	minDist, this);
+	}
 	
 	
+	@Override
+	public void onLocationChanged(Location location) {
+		// TODO Auto-generated method stub
+		log("Nueva localizaciÛn: ");
+
+		if (pointsCuantity == 1) {
+			
+			lastLoc = new Location(location);
+			
+		}
+
+		muestraLocaliz(location);
+	}
+
+	@Override
+	public void onProviderDisabled(String proveedor) {
+		// TODO Auto-generated method stub
+		log("Proveedor deshabilitado: " + proveedor + "\n");
+
+	}
+
+	@Override
+	public void onProviderEnabled(String proveedor) {
+		// TODO Auto-generated method stub
+		log("Proveedor habilitado: " + proveedor + "\n");
+
+	}
+
+	@Override
+	public void onStatusChanged(String proveedor, int estado, Bundle extras) {
+ 
+		// TODO Auto-generated method stub
+		log("Cambia estado proveedor: " + proveedor + ", estado="
+				+ E[Math.max(0, estado)] + "\n");
+	}
 	
+	
+	// MÈtodos para mostrar informaciÛn
+
+		private void log(String cadena) {
+
+			//salida.append(cadena + "\n");
+
+		}
+
+		private void muestraLocaliz(Location localizacion) {
+
+			if (localizacion == null)
+
+				log("LocalizaciÛn desconocida\n");
+
+			else
+
+			if (pointsCuantity <= 1) {
+				distance = 0;
+
+				if (pointsCuantity == 1)
+					log("FirstLocation");
+
+				if (pointsCuantity == 0)
+					log("LastKnownLocation");
+			}
+
+			else {
+				distance = localizacion.distanceTo(lastLoc);
+				lastLoc = new Location(localizacion);
+			}
+
+			
+			
+			//los datos a guardar son los sigientes, mas la distance (0 para el primer punto y la diferencia entre los anteriores para el resto
+			//solo se necesita guardar a partir de pointsCuantity = 1, la que el 0 es el LastKnownLocation que es la cacheada
+			latitude = localizacion.getLatitude();
+			altitude = localizacion.getAltitude();
+			longitude = localizacion.getLongitude();
+			velocity = localizacion.getSpeed();
+			time = localizacion.getTime();
+
+			log("Localizacion[ " + "Orden=" + pointsCuantity + ", Latitud(grados)="
+					+ latitude +
+
+					", Longitud(grados)=" + longitude +
+
+					", Altitud(metros)=" + altitude +
+
+					", Velocidad(m/s)=" + velocity +
+
+					", Distancia(metros)=" + velocity +
+
+					", Tiempo(UTC millisegundos)=" + time + " ]\n");
+			
+			pointsCuantity++;
+
+		}
+	
+		public float getLastDistance(){
+			return distance;
+		}
+		public float getLastSpeed(){
+			return velocity;
+		}
 }
